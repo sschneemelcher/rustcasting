27b1a116c4512c351da01af90ffb8002b98358f9
Simon Schneemelcher <s.schneemelcher@tu-bs.de>
2021-02-14 18:59:29 +0100

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -33,24 +33,22 @@ impl Game {
 	}
 
 //	fn update(&mut self) {
+
 //	}
 
 	fn pressed(&mut self, btn: &Button) {
 		let last_direction = self.player.dir.clone();
 
 		self.player.dir = match btn {
-			&Button::Keyboard(Key::Up)
-				if last_direction != Direction::Down => Direction::Up,
-			&Button::Keyboard(Key::Down)
-				if last_direction != Direction::Up => Direction::Down,
-			&Button::Keyboard(Key::Left)
-				if last_direction != Direction::Right => Direction::Left,
-			&Button::Keyboard(Key::Right)
-				if last_direction != Direction::Left => Direction::Right,
+			&Button::Keyboard(Key::Up) => Direction::Up,
+			&Button::Keyboard(Key::Down) => Direction::Down,
+			&Button::Keyboard(Key::Left) => Direction::Left,
+			&Button::Keyboard(Key::Right) => Direction::Right,
 			_ => last_direction
 		};
-		
-		self.player.update();
+			
+		self.player.update(&self.map);
+		self.view.update(&self.player);
 	}
 }
 
@@ -63,12 +61,37 @@ struct Player {
 }
 
 impl Player {
-	fn update(&mut self) {
+	fn update(&mut self, map: &Map) {
+		let deg:f32 = 3.14159/30.0;
+		let speed:f32 = 0.05;
+
 		match self.dir {
-			Direction::Left => self.pos_x -= 1.0,
-			Direction::Right => self.pos_x += 1.0,
-			Direction::Up => self.pos_y -= 1.0,
-			Direction::Down => self.pos_y += 1.0,
+			Direction::Left => {
+				let old_dir_x:f32 = self.dir_x.clone();
+				self.dir_x = old_dir_x * deg.cos() - self.dir_y * deg.sin();
+				self.dir_y = old_dir_x * deg.sin() + self.dir_y * deg.cos();
+				},
+			Direction::Right => {
+				let old_dir_x:f32 = self.dir_x.clone();
+				self.dir_x = old_dir_x * (-deg).cos() - self.dir_y * (-deg).sin();
+				self.dir_y = old_dir_x * (-deg).sin() + self.dir_y * (-deg).cos();
+				},
+			Direction::Up => {
+				if self.pos_y > self.dir_y * speed{
+					self.pos_y += self.dir_y * speed;
+				}
+				if self.pos_x > self.dir_x * speed {
+					self.pos_x += self.dir_x * speed;
+				}
+				},
+			Direction::Down => {
+				if self.pos_y + self.dir_y * speed < map.height as f32 {
+					self.pos_y -= self.dir_y * speed;
+				}
+				if self.pos_x + self.dir_x * speed < map.width as f32 {
+					self.pos_x -= self.dir_x * speed;
+				}
+				}, 
 		}
 	}
 }
@@ -81,6 +104,8 @@ struct Map {
 
 
 struct View {
+	screen_width: i32,
+	screen_height: i32,
 	plane_x: f32,
 	plane_y: f32,
 	max_dof: u8,
@@ -96,6 +121,16 @@ fn sign(x:f32) -> f32 {
 
 impl View {
 	fn render(&self, gl: &mut GlGraphics, arg: &RenderArgs, player: &Player, map: &Map) {
+
+
+		let black: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+
+		gl.draw(arg.viewport(), |_c, gl| {
+			graphics::clear(black, gl);
+		});
+
+
+
 		let w:i32 = map.width*self.rays;
 		for x in 1..w {
 			let camera_x:f32 = 2.0 * x as f32 / self.rays as f32 / map.width as f32 - 1.0;
@@ -123,6 +158,7 @@ impl View {
 
 			let mut side:i32 = 0;
 			let mut field_value:i32 = 0;
+			let mut perp_wall_dist:f32 = 0.0;
 			while hit == 0 && dof < self.max_dof {
 				side = (side_dist_y < side_dist_x) as i32;
 				if side == 1 {
@@ -132,35 +168,68 @@ impl View {
 					map_x = (map_x + steps_x as i32) % map.width;
 					side_dist_x += delta_dist_x;
 				}
-				if (map_y * map.width + map_x) < map.width * map.height {
+				if 0 < (map_y * map.width + map_x) && (map_y * map.width + map_x) < map.width * map.height {
 					field_value = map.values[(map_y*map.width+map_x) as usize];
+					if field_value > 0 {
+						hit = 1;
+					}	
 				} else {
 					field_value = 1;
+					hit = 1;
 				}
 				
-				if field_value > 0 {
-					hit = 1;
-				}	
 				dof += 1;
 			}
+			if hit == 0 {
+				continue;
+			}
 
-		}
+			if side == 1 {
+				perp_wall_dist = (map_y as f32 - player.pos_y + (1.0 - steps_y) * 0.5) / raydir_y;
+			} else {
+				perp_wall_dist = (map_x as f32 - player.pos_x + (1.0 - steps_x) * 0.5) / raydir_x;
+			}
 
-				
+			let mut lh:f32 = self.screen_height as f32;
+			if perp_wall_dist > 0.0 {
+				lh /= perp_wall_dist;
+			}
 
-
-
-
-		let red: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-		let square = graphics::rectangle::square((player.pos_x * 10.0) as f64,
-			(player.pos_y * 10.0) as f64,
-			20_f64);
+			let lw:i32 = self.screen_width / w;
+			
+			let red: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+			
+			let square = graphics::rectangle::rectangle_by_corners((x * lw) as f64,
+								((self.screen_height as f32 - lh) * 0.5) as f64,
+								((x * lw) + lw) as f64, ((self.screen_height as f32 - lh) * 0.5 + lh) as f64);
 	
-		gl.draw(arg.viewport(), |c, gl| {
-			let transform = c.transform;
-			graphics::rectangle(red, square, transform, gl);
-		});
+			gl.draw(arg.viewport(), |c, gl| {
+				let transform = c.transform;
+				graphics::rectangle(red, square, transform, gl);
+			});
+		}
 	}
+
+	fn update(&mut self, player: &Player) {
+		let deg:f32 = 3.14159/30.0;
+
+		match player.dir {
+			Direction::Left => {
+				let old_plane_x:f32 = self.plane_x.clone();
+				self.plane_x = old_plane_x * deg.cos() - self.plane_y * deg.sin();
+				self.plane_y = old_plane_x * deg.sin() + self.plane_y * deg.cos();
+				},
+			Direction::Right => {
+				let old_plane_x:f32 = self.plane_x.clone();
+				self.plane_x = old_plane_x * (-deg).cos() - self.plane_y * (-deg).sin();
+				self.plane_y = old_plane_x * (-deg).sin() + self.plane_y * (-deg).cos();
+				},
+			_ => (),
+		}
+	}
+
+
+
 }
 
 
@@ -169,7 +238,7 @@ fn main() {
 
 	let mut window: Window = WindowSettings::new(
 		"raycasting",
-		[800, 800]
+		[960, 480]
 		).graphics_api(opengl)
 		.exit_on_esc(true)
 		.build()
@@ -177,9 +246,9 @@ fn main() {
 
 	let mut game = Game {
 		gl: GlGraphics::new(opengl),
-		player: Player { pos_x: 1.0, pos_y: 1.0, dir_x: -1.0, dir_y: 0.0, dir: Direction::Right },
-		map: Map { height: 3, width: 3, values: [1,1,1,1,0,1,1,1,1].to_vec() },
-		view: View { plane_x: 0.0, plane_y:0.66, max_dof: 8, rays: 16 },
+		player: Player { pos_x: 2.0, pos_y: 2.0, dir_x: -1.0, dir_y: 0.0, dir: Direction::Right },
+		map: Map { height: 5, width: 5, values: [1,1,1,1,1,1,0,0,0,1,1,0,1,0,1,1,0,0,0,1,1,1,1,1,1].to_vec() },
+		view: View { screen_width: 960, screen_height: 480, plane_x: 0.0, plane_y:0.66, max_dof: 5, rays: 24 },
 	};
 
 	let mut events = Events::new(EventSettings::new());//.ups(120);
@@ -187,15 +256,14 @@ fn main() {
 		if let Some(args) = e.render_args() {
 			game.render(&args);
 		}
-		/*
-		if let Some(_u) = e.update_args() {
-			game.update();
-		}
-		*/
+		
+	//	if let Some(_u) = e.update_args() {
+	//		game.update();
+	//	}
 		if let Some(k) = e.button_args() {
 			if k.state == ButtonState::Press {
 				game.pressed(&k.button);
-		}
+			}
 		}
 
 	}
